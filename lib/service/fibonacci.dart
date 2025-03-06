a236665b61e27b957279109d57d87683266edc4a
LucassSantoss <lucascross.pereirasantos@gmail.com>
2025-03-06 10:32:47 -0300

    feat: uses dynamic programming to calculate fibonacci

--- lib/service/fibonacci.dart
+++ lib/service/fibonacci.dart
@@ -1,3 +1,4 @@
+// O(2^n)
 int fibonaci(int num) {
   if (num <= 0) {
     throw Exception("invalid number: $num");
@@ -5,4 +6,27 @@ int fibonaci(int num) {
   if (num == 1) return 1;
   if (num == 2) return 1;
   return fibonaci(num - 1) + fibonaci(num - 2);
+}
+
+// Programação dinâmica bottom-up
+// O(n)
+BigInt dynamicFibonacci(int n) {
+  if (n <= 0) {
+    throw Exception("invalid number: $n");
+  }
+  if (n == 1 || n == 2) return BigInt.one;
+
+  List<BigInt> list = List.filled(n + 1, BigInt.zero);
+  list[1] = BigInt.one;
+
+  for (int i = 2; i <= n; i++) {
+    list[i] = list[i - 1] + list[i - 2];
+  }
+
+  return list[n];
+}
+
+void main() {
+  //print(fibonaci(60));
+  print(dynamicFibonacci(120));
 }
